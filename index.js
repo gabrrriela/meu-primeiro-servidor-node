f347adf56e4fa4b70673903fa72d93db2d3e5667
Gabriela Amorim <gabriela.amorim@outlook.com.br>
2021-07-26 21:30:44 -0300

    feature: responde com html

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-        res.end('Servidor funcionando!')
+    res.writeHead (200,{
+        'Content-Type': 'text/html; charset=utf-8'
+    })
+        res.end('<h1>Servidor funcionando redondinho, ó!')
     }).listen(3000, 'localhost', () => console.log("O server está on..."))
